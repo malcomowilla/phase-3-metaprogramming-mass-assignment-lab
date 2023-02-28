4d041e948be667f4dbe3190aa92273861861a8e6
malcomowilla <owillamalcom@gmail.com>
2023-02-28 06:27:51 +0300

    metaprogramming

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
+
+
+# Person
+# with a longer list of attributes
+# will assign an arbitrary number of properties on initialization
+# with a different, shorter list of attributes
+# will also assign an arbitrary number of properties on initialization
+
+
+
+
+
+
+
 class Person
   # your code here
+  def initialize(attributes = nil)
+    if attributes
+      attributes.each do |key, value|
+        self.class.attr_accessor(key)
+        self.send("#{key}=", value)
+      end
+    end
+  end
 end
