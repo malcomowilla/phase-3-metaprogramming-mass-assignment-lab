e40f2ed8526bbd22e64c0a6c9efcc853131cc098
malcomowilla <owillamalcom@gmail.com>
2023-06-13 06:53:05 +0300

    solved

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,34 @@
 
 
+
+
+class Person
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
+end
+
+bob_attributes = {name:"Bob", hair_color:"Brown"}
+puts bob = Person.new(bob_attributes)
+puts bob.name
+puts bob.hair_color
+
+susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+susan = Person.new(susan_attributes)
+puts susan.height
+
+
+
+
+
+
+
+
+
+
 # Person
 # with a longer list of attributes
 # will assign an arbitrary number of properties on initialization
